dbcc9bc3ab2f6b3bfd2c2c36ffa45b84dd35a067
SebastianBR86 <sroby1@my.bcit.ca>
2026-05-07 13:26:17 -0700

    moved images, css, and created a js folder in a public folder for EJS. created basic app functionality in app.js. created recipe.ejs page, currently empty besides header and footer.

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+// Middleware to serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Index
+app.get('/', (req, res) => {
+    res.render('recipes', {
+        cssFiles: ['style']
+    });
+});
+
+
+
+
+
+
+
+
+// 404
+app.use((req,res) => {
+	res.status(404);
+	res.send("Page not found - 404");
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
