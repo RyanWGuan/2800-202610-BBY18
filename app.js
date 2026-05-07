8993ca0c518004be743af0c59989704826e03f9e
Bennett Lazarro <lazarrobennett19@gmail.com>
2026-05-07 14:52:59 -0700

    added scripts folder in public

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Middleware to serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Index
-app.get('/', (req, res) => {
-    res.render('recipes', {
-        cssFiles: ['style']
-    });
+app.get("/", (req, res) => {
+  res.render("recipes", {
+    cssFiles: ["style"],
+  });
 });
 
-app.get('/profile', (req,res) => {
-    res.render('profile');
+app.get("/profile", (req, res) => {
+  res.render("profile");
 });
 
-app.get('/map', (req,res) => {
-    res.render('map');
+app.get("/map", (req, res) => {
+  res.render("map");
 });
 
-app.get('/ingredients', (req,res) => {
-    res.render('ingredients');
+app.get("/ingredients", (req, res) => {
+  res.render("ingredients", {
+    title: "Ingredients",
+    cssFiles: ["style"],
+    jsFiles: [],
+  });
 });
 
 // 404
-app.use((req,res) => {
-	res.status(404);
-	res.send("Page not found - 404");
+app.use((req, res) => {
+  res.status(404);
+  res.send("Page not found - 404");
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
