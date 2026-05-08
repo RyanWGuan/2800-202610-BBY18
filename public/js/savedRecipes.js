da0dfe34f30c42812ebaf34005edb9bdf3420c80
Minsung Park <nevgivup.park@gmail.com>
2026-05-08 15:12:09 -0700

    Pop up added for savedLocations and savedRecipes

---
+++ public/js/savedRecipes.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const popup = document.getElementById("savedRecipesPopup");
+    const closeBtn = document.getElementById("closeSavedRecipesPopup");
+
+    const hasVisitedSavedRecipes = localStorage.getItem("hasVisitedSavedRecipes");
+
+    if (!hasVisitedSavedRecipes) {
+        popup.style.display = "flex";
+    }
+
+    closeBtn.addEventListener("click", () => {
+        popup.style.display = "none";
+        localStorage.setItem("hasVisitedSavedRecipes", "true");
+    });
+});
