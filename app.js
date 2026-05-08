69251ca3e19ae240889b68e2983b005faee6a07c
Dev-L-88 <devanlam88@gmail.com>
2026-05-08 12:11:24 -0700

    moved all js files to the js folder, and fixed button functionality

--- app.js
+++ app.js
@@ -18,19 +18,22 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render('login', {
-        cssFiles: ['login']
+        cssFiles: ['login'],
+        jsFiles: ['login']
     });
 });
 
 app.get('/profile', (req,res) => {
     res.render('profile', {
-        cssFiles: ['style']
+        cssFiles: ['style'],
+        jsFiles: ['profile']
     });
 });
 
 app.get('/map', (req,res) => {
     res.render('map', {
         cssFiles: ['style'],
+        jsFiles: ['map'],
         mapboxToken: process.env.MAPBOX_TOKEN
     });
 });
@@ -40,11 +43,17 @@ app.get('/ingredients', (req,res) => {
 });
 
 app.get("/savedLocations", (req, res) => {
-    res.render("savedLocations", { cssFiles: ["style"] });
+    res.render("savedLocations", { 
+        cssFiles: ["style"],
+        jsFiles: []
+    });
 });
 
 app.get("/savedRecipes", (req, res) => {
-    res.render("savedRecipes", { cssFiles: ["style"] });
+    res.render("savedRecipes", { 
+        cssFiles: ["style"],
+        jsFiles: []
+    });
 });
 
 // 404
