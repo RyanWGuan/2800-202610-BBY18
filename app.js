ab0c724547a34939ce248cfb34a5f274a3a16124
Dev-L-88 <devanlam88@gmail.com>
2026-05-07 22:32:09 -0700

    completed the profile page

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/profile', (req,res) => {
-    res.render('profile');
+    res.render('profile', {
+        cssFiles: ['style']
+    });
 });
 
 app.get('/map', (req,res) => {
