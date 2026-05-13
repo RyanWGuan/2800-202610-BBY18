5f63f6a352ade1f5139eb3d6c617c4119f611a74
Ryan Guan <ryanwguan@gmail.com>
2026-05-13 14:20:12 -0700

    Added some code relating to session handling.

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const session = require("express-session");
+const MongoStore = require("connect-mongo").default;
 const port = process.env.PORT || 3000;
 
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+
+const { MongoClient } = require('mongodb');
+
+
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/`;
+const database = new MongoClient(atlasURI, {});
+const userCollection = database.db(mongodb_database).collection('users');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
 app.set("view engine", "ejs");
+
+var mongoStore = MongoStore.create({
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`,
+    crypto: {
+        secret: mongodb_session_secret
+    }
+});
+
+app.use(session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: false,
+    cookie: true,
+}));
+
 
 // Middleware to serve static files from the 'public' directory
 app.use(express.static("public"));
