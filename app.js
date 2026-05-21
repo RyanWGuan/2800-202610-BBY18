1f351ba5d0c9ca9a153641d27c91085769671449
Ryan Guan <ryanwguan@gmail.com>
2026-05-20 19:18:18 -0700

    Fixed Users being stored in wrong database for sure this time

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const Joi = require("joi");
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
-const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_database = process.env.MONGODB_USER_DATABASE;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 
