012585f3e76e13a7a46276e1dff88474f2a6f6ee
Ryan Guan <ryanwguan@gmail.com>
2026-05-22 09:42:41 -0700

    Commented Login JS

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,16 @@
+//Toggle to show signup panel
 function showSignup() {
     document.getElementById('login_container').style.display = 'none';
     document.getElementById('signup_container').style.display = 'flex';
 }
 
+//Toggle to show login panel
 function showLogin() {
     document.getElementById('signup_container').style.display = 'none';
     document.getElementById('login_container').style.display = 'flex';
 }
 
+//Listener to check if user visited for the first time ever, display popup if they are first time.
 document.addEventListener("DOMContentLoaded", () => {
     const popup = document.getElementById('firstTimePopup');
     const closeBtn = document.getElementById('closePopup');
